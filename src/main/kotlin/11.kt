c81f9c6fa0b8e81314901efaa2f0810260a97d27
Conor Griffiths <Conor.Griffiths@anz.com>
2022-12-12 10:03:53 +1300

    Refactored 11 to replace naive BigInt solution

--- src/main/kotlin/11.kt
+++ src/main/kotlin/11.kt
@@ -1,14 +1,13 @@
-import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
 
 data class Monkey(
-    val items : MutableList<BigInteger>,
+    val items : MutableList<Long>,
     val opSymbol : Char,
-    val opNum: BigInteger,
+    val opNum: Long,
     val testSymbol: Char,
-    val testNum: Int,
+    val testNum: Long,
     val throwTarget: Pair<Int, Int>,
     var numInspected: Int = 0
     ){
@@ -17,13 +16,13 @@ data class Monkey(
             numInspected++
             var item = items[0]
             items.removeAt(0)
-            val tempOpNum = if (opNum != (0.toBigInteger())) opNum else item
+            val tempOpNum = if (opNum != 0L) opNum else item
             when (opSymbol) {
-                '+' -> item = (item.plus(tempOpNum)).mod(Monkeys.modulus)
-                '*' -> item = (item * tempOpNum).mod(Monkeys.modulus)
+                '+' -> item = (item + tempOpNum) % (Monkeys.modulus)
+                '*' -> item = (item * tempOpNum) % (Monkeys.modulus)
             }
             //item /= 3
-            val targetMonkey = if(item.remainder(testNum.toBigInteger()) == 0.toBigInteger()) throwTarget.first else throwTarget.second
+            val targetMonkey = if(item % testNum == 0L) throwTarget.first else throwTarget.second
             Monkeys.passToMonkey(targetMonkey, item)
         }
 
@@ -38,9 +37,9 @@ class Monkeys() {
             val sc = Scanner(monkeyString.trim())
             if(sc.hasNextLine()) {
                 sc.nextLine()
-                val items = mutableListOf<BigInteger>()
+                val items = mutableListOf<Long>()
                 val itemString = sc.nextLine().split(":")[1].trim().split(", ")
-                itemString.forEach{items.add(it.toBigInteger())}
+                itemString.forEach{items.add(it.toLong())}
                 val op = sc.nextLine().split("=")[1].trim().split(" ")
                 val opSymbol = op[1].trim()
                 val opNum = op[2].trim().toIntOrNull() ?: 0
@@ -49,7 +48,7 @@ class Monkeys() {
                 val testNum = testLine[testLine.size-1].trim().toInt()
                 val trueTarget = sc.nextLine().split("monkey")[1].trim().toInt()
                 val falseTarget = sc.nextLine().split("monkey")[1].trim().toInt()
-                val monkey = Monkey(items, opSymbol[0], opNum.toBigInteger(), testSymbol[0], testNum, Pair(trueTarget, falseTarget))
+                val monkey = Monkey(items, opSymbol[0], opNum.toLong(), testSymbol[0], testNum.toLong(), Pair(trueTarget, falseTarget))
                 monkeys.add(monkey)
             }
         }
@@ -60,8 +59,8 @@ class Monkeys() {
             monkeys.forEach{it.takeTurn()}
             if(i%20==0){
                 println("Turn $i")
-                for (i in 0 until monkeys.size){
-                    println("Monkey $i inspected ${monkeys[i].numInspected} items")
+                for (j in 0 until monkeys.size){
+                    println("Monkey $j inspected ${monkeys[j].numInspected} items")
                 }
             }
         }
@@ -75,9 +74,9 @@ class Monkeys() {
 
     companion object {
         private val monkeys = mutableListOf<Monkey>()
-        val modulus = 9699690.toBigInteger() //Product of multiplying all the divisors
+        const val modulus = 9699690L //Product of multiplying all the divisors
 
-        fun passToMonkey(monkeyNum: Int, item: BigInteger){
+        fun passToMonkey(monkeyNum: Int, item: Long){
             monkeys[monkeyNum].items.add(item)
         }
     }
@@ -87,11 +86,7 @@ class Monkeys() {
 fun main(){
     val file = Files.readString(Path.of("11.txt"))
     val monkeys = Monkeys(file)
-
     monkeys.runGame(10000)
-
-
-
 }
 
 
